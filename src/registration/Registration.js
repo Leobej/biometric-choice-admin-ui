ab361ff6f42072aacf41b394cdda32a33b230420
Bejgu Leonard Mihai <61775738+Leobej@users.noreply.github.com>
2023-04-15 17:29:05 +0300

    Add basic login and registration

---
+++ src/registration/Registration.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Registration() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-6">Fingerprint Registration</h1>
+      <form className="bg-white rounded-lg p-6 shadow-md">
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="first_name">
+            First Name
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="first_name"
+            type="text"
+            placeholder="John"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="last_name">
+            Last Name
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="last_name"
+            type="text"
+            placeholder="Doe"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="date_of_birth">
+            Date of Birth
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="date_of_birth"
+            type="date"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="cnp">
+            CNP
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="cnp"
+            type="text"
+            placeholder="1234567890123"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="serie">
+            Serie
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="serie"
+            type="text"
+            placeholder="AA123456"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="fingerprint">
+            Fingerprint
+          </label>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Scan Fingerprint
+          </button>
+        </div>
+        <div className="flex items-center justify-between">
+          <Link to="/login">
+            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Cancel
+            </button>
+          </Link>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Registration;
